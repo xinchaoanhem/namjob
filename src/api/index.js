003418a9f5a65083ab896e663f516780cd8f9c49
namlt <tuannam2505@gmail.com>
2022-12-15 11:19:09 +0700

    update 2

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from "../store";
 import router from "../router";
 
-const baseUrl = 'http://127.0.0.1:8000';
+const baseUrl = 'https://namhihi.000webhostapp.com';
 
 export const apiAxios = axios.create({
     baseURL:`${baseUrl}/api/`,
